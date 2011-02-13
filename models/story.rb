243801a66ddd811a76414471ccd8b7245035bb72
Wishpot <ops@wishpot.com>
2011-02-12 20:37:35 -0800

    Added dates to the report so you can see how old the story was.

--- models/story.rb
+++ models/story.rb
@@ -1,6 +1,6 @@
 
 class Story
-attr_reader :title, :description, :url, :accepted_at, :requested_by, :owned_by
+attr_reader :title, :description, :url, :accepted_at, :requested_by, :owned_by, :created_at
   
 # Builds a story given an XML node from pivotal tracker
 def from_xml(node)
@@ -8,6 +8,7 @@ def from_xml(node)
   @description = node.xpath('description')[0].content
   @url = node.xpath('url')[0].content
   @accepted_at = DateTime.parse(node.xpath('accepted_at')[0].content)
+  @created_at = DateTime.parse(node.xpath('created_at')[0].content)
   @requested_by = node.xpath('requested_by')[0].content
   @owned_by = node.xpath('owned_by')[0].content
   self
