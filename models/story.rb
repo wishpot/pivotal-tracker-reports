6617182110b5e078962708570bc9243a4be59b86
Tom Lianza <tom@wishpot.com>
2011-08-10 10:24:40 -0700

    Improved the presentation of upcoming stories, with more relevant info

--- models/story.rb
+++ models/story.rb
@@ -1,6 +1,6 @@
 
 class Story
-attr_reader :id, :title, :description, :url, :accepted_at, :requested_by, :owned_by, :created_at, :story_type
+attr_reader :id, :title, :description, :url, :accepted_at, :requested_by, :owned_by, :created_at, :story_type, :current_state
   
 # Builds a story given an XML node from pivotal tracker
 def from_xml(node)
@@ -12,6 +12,7 @@ def from_xml(node)
   @accepted_at = accepted_at_node.nil? ? nil : DateTime.parse(accepted_at_node.content)
   @created_at = DateTime.parse(node.xpath('created_at')[0].content)
   @requested_by = node.xpath('requested_by')[0].content
+  @current_state = node.xpath('current_state')[0].content
   owned_by_node = node.xpath('owned_by')[0]
   @owned_by = owned_by_node.nil? || owned_by_node.content.length == 0 ? 'no one' : owned_by_node.content
   @story_type = node.xpath('story_type')[0].content
@@ -25,6 +26,13 @@ end
 #handy for turning labels into something more human readable
 def self.human_format(str)
   str.gsub('z_', '').gsub(/[_-]/, ' ').capitalize
+end
+
+#This is the label for status that we show in the report
+def friendly_state
+  return '' if current_state == 'unstarted'
+  return 'in progress' if current_state == 'started'
+  current_state
 end
 
 #Simplified version from the rails source
